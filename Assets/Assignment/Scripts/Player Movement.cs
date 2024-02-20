107ccb4fc4d3faf8827efda2e9990312d5f72fcc
Clamel05 <caml@bell.net>
2024-02-19 20:38:38 -0500

    Created a trigger in Scene1 which brings you to the next secen when the player enters it. Also created a button in Scene 2 which brings you back to Scene 1 and restarts the game.

--- Assets/Assignment/Scripts/Player Movement.cs
+++ Assets/Assignment/Scripts/Player Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -41,6 +42,17 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
-
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+
+
 }
